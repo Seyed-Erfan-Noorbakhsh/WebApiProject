a9392a6199e5c95488fbe72f14f3ce202c6fd0fc
Mohammad Javad <mohaadjavad.2005@gmail.com>
2025-12-25 01:51:24 +0330

    add Navigation Property  to entities -> order.cs

--- Shop_ProjForWeb/Core/Domain/Entities/Order.cs
+++ Shop_ProjForWeb/Core/Domain/Entities/Order.cs
@@ -1,10 +1,15 @@
 namespace Shop_ProjForWeb.Core.Domain.Entities;
-using Domain.Enums;
+using Shop_ProjForWeb.Core.Domain.Enums;
 
-public class Order : BaseEntity
+
+public class Order
 {
+    public Guid Id { get; set; } = Guid.NewGuid();
     public Guid UserId { get; set; }
     public OrderStatus Status { get; set; }
     public decimal TotalPrice { get; set; }
     public DateTime? PaidAt { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
 }
