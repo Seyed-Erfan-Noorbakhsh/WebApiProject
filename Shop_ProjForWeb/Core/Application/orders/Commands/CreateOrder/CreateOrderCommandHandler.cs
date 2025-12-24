35d0c2f3e44cd4e0531439b79494b1e6cc8e7172
Mohammad Javad <mohaadjavad.2005@gmail.com>
2025-12-25 01:26:04 +0330

    improve the code of create order command handler and add order

--- Shop_ProjForWeb/Core/Application/orders/Commands/CreateOrder/CreateOrderCommandHandler.cs
+++ Shop_ProjForWeb/Core/Application/orders/Commands/CreateOrder/CreateOrderCommandHandler.cs
@@ -1,26 +1,63 @@
 using MediatR;
 using Shop_ProjForWeb.Core.Application.DTOs;
 using Shop_ProjForWeb.Core.Application.Interfaces;
+using Shop_ProjForWeb.Core.Domain.Entities;
+using Shop_ProjForWeb.Core.Domain.Enums;
 
 namespace Shop_ProjForWeb.Core.Application.Orders.Commands.CreateOrder
 {
     public class CreateOrderCommandHandler
         : IRequestHandler<CreateOrderCommand, OrderResponseDto>
     {
-        private readonly IOrderService _orderService;
+         private readonly IUserRepository _userRepository;
+        private readonly IProductRepository _productRepository;
+        private readonly IInventoryRepository _inventoryRepository;
+        private readonly IOrderRepository _orderRepository;
+        private readonly PricingService _pricingService;
+        private readonly InventoryService _inventoryService;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public CreateOrderCommandHandler(IOrderService orderService)
+        public CreateOrderCommandHandler(
+             IUserRepository userRepository,
+            IProductRepository productRepository,
+            IInventoryRepository inventoryRepository,
+            IOrderRepository orderRepository,
+            PricingService pricingService,
+            InventoryService inventoryService,
+            IUnitOfWork unitOfWork)
+        
         {
-            _orderService = orderService;
+            _userRepository = userRepository;
+            _productRepository = productRepository;
+            _inventoryRepository = inventoryRepository;
+            _orderRepository = orderRepository;
+            _pricingService = pricingService;
+            _inventoryService = inventoryService;
+            _unitOfWork = unitOfWork;
         }
 
         public async Task<OrderResponseDto> Handle(
             CreateOrderCommand request,
             CancellationToken cancellationToken)
         {
-            return await _orderService.CreateOrderAsync(
-                request.UserId,
-                request.Items);
+            var user = await _userRepository.GetByIdAsync(request.UserId);
+            if (user == null)
+                throw new Exception($"User not found with id {request.UserId}");
+
+            var order = new Order
+            {
+                UserId = request.UserId,
+                Status = OrderStatus.Created,
+                TotalPrice = 0
+            };
+            decimal totalPrice = 0;
+
+            foreach (var item in request.Items)
+            {
+                
+            }
+
+            
         }
     }
 }
