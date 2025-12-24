b6beac98a2f96b4818a4b5d91efa6413553220b7
Seyed-Erfan-Noorbakhsh <126964073+Seyed-Erfan-Noorbakhsh@users.noreply.github.com>
2025-12-24 22:15:38 +0330

    last adding of first try ( ERFAN )

---
+++ Shop_ProjForWeb/Infrastructure/Middleware/SecurityHeadersMiddlewareExtensions.cs
@@ -0,0 +1,10 @@
+namespace Shop_ProjForWeb.Infrastructure.Middleware;
+
+public static class SecurityHeadersMiddlewareExtensions
+{
+    public static IApplicationBuilder UseSecurityHeaders(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<SecurityHeadersMiddleware>();
+    }
+}
+
