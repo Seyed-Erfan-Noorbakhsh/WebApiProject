c02a3a88ec73d89a3048309adfa2e6b7dac67330
Mohammad javad Rezaei <mohammadjavadrezaei333@gmail.com>
2025-12-31 13:35:33 +0330

    build the entities in Domain folder

---
+++ Core/Domain/Entities/BaseEntity.cs
@@ -0,0 +1,10 @@
+namespace Shop_ProjForWeb.Core.Domain.Entities;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public bool IsDeleted { get; set; } = false;
+    public DateTime? DeletedAt { get; set; }
+}
