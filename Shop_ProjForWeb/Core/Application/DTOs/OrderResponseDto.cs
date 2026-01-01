d26b7885fc044eeffd820b43d971a7d804db9488
Seyed Erfan Noorbakhsh <seyederfan.noorbakhsh@gmail.com>
2026-01-01 18:51:54 +0330

    adding orders part for dtos

---
+++ Shop_ProjForWeb/Core/Application/DTOs/OrderResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Shop_ProjForWeb.Core.Application.DTOs;
+
+using Shop_ProjForWeb.Core.Domain.Enums;
+
+public class OrderResponseDto
+{
+    public Guid OrderId { get; set; }
+    public decimal TotalPrice { get; set; }
+    public OrderStatus Status { get; set; }
+}
+
