7d47b80a6c957d6a837b0474788b1be3b17c27bd
Seyed Erfan Noorbakhsh <seyederfan.noorbakhsh@gmail.com>
2026-01-01 18:50:18 +0330

    adding inventory part in DTOs

---
+++ Shop_ProjForWeb/Core/Application/DTOs/ProductDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Shop_ProjForWeb.Core.Application.DTOs;
+
+public class ProductDto
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+    public decimal BasePrice { get; set; }
+    public int DiscountPercent { get; set; }
+    public bool IsActive { get; set; }
+    public string? ImageUrl { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
