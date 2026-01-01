8df8ce5dd09cbee2bd86eadcb7cbeeb40e1585a2
Mohammad javad Rezaei <mohammadjavadrezaei333@gmail.com>
2026-01-01 18:10:40 +0330

    add validate entity method to order item

--- Core/Domain/Entities/OrderItem.cs
+++ Core/Domain/Entities/OrderItem.cs
@@ -12,4 +12,20 @@ public class OrderItem : BaseEntity
     // Navigation Properties
     public Order? Order { get; set; }
     public Product? Product { get; set; }
+
+    public override void ValidateEntity()
+    {
+        base.ValidateEntity();
+        
+        if (OrderId == Guid.Empty)
+            throw new ArgumentException("OrderId cannot be empty");
+        
+        if (ProductId == Guid.Empty)
+            throw new ArgumentException("ProductId cannot be empty");
+        
+        ValidateDecimalProperty(UnitPrice, nameof(UnitPrice), minValue: 0);
+        ValidateIntProperty(Quantity, nameof(Quantity), minValue: 1);
+        ValidateIntProperty(ProductDiscountPercent, nameof(ProductDiscountPercent), minValue: 0, maxValue: 100);
+        ValidateIntProperty(VipDiscountPercent, nameof(VipDiscountPercent), minValue: 0, maxValue: 100);
+    }
 }
