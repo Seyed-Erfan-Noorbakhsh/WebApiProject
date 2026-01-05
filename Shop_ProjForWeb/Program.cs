256c33d278d0fcf72a29f858554dbd2191650d12
taha <stmh1363@gmail.com>
2026-01-05 16:01:45 +0330

    t

--- Shop_ProjForWeb/Program.cs
+++ Shop_ProjForWeb/Program.cs
@@ -12,7 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<SupermarketDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseInMemoryDatabase("SupermarketDb"));
 
 // Register Repositories
 builder.Services.AddScoped<IUserRepository, UserRepository>();
